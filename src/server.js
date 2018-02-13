4d20efe97bba5a43d7857285421856f6b8c6580e
Paul-Adrien Bru <p.a.bru78@gmail.com>
2018-02-13 15:19:58 +0100

    added express to expose the graphql API on /graphql endpoint

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import { graphql, buildSchema } from 'graphql'
-
-const query = '{ hello }'
+import express from 'express'
+import bodyParser from 'body-parser'
 
 const schema = buildSchema(`
 type Query {
@@ -14,7 +14,22 @@ const resolvers = {
   },
 }
 
-graphql(schema, query, resolvers).then(response => {
-  console.info(response)
-  return response
+const app = express()
+app.use(bodyParser.json())
+app.use('/graphql', (req, res) => {
+  console.log('Query received', req.body.query)
+  graphql(schema, req.body.query, resolvers).then(result => {
+    console.log('result', result)
+    res.send(result)
+  })
 })
+
+app.listen(4000, () =>
+  console.log('App is listening on localhost:4000/graphql')
+)
+
+// Now try this request:
+// curl -X POST \
+//   http://localhost:4000/graphql \
+//   -H 'content-type: application/json' \
+//   -d '{"query":"{hello}"}'
