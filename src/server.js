0d7105a0182765dad0d7800f1f4685ebe5d16a16
Paul-Adrien Bru <p.a.bru78@gmail.com>
2018-02-13 16:10:40 +0100

    added http graphql middleware for express

--- src/server.js
+++ src/server.js
@@ -1,35 +1,62 @@
 import { graphql, buildSchema } from 'graphql'
 import express from 'express'
 import bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 
 const schema = buildSchema(`
+type User {
+  id: ID!
+  username: String!
+  age: Int!
+  bio: String
+}
+
 type Query {
   hello: String
+
+  user ( id: ID): User
 }
 `)
+
+const fakeDb = {
+  users: [
+    {
+      id: 1,
+      username: 'Polo',
+      bio: 'The bio of Polo',
+      age: 22,
+    },
+    {
+      id: 2,
+      username: 'Chuck',
+      bio: 'The bio of Chuck',
+      age: 34,
+    },
+    {
+      id: 3,
+      username: 'Ricky',
+      bio: 'The bio of Ricky',
+      age: 56,
+    },
+  ],
+}
 
 const resolvers = {
   hello() {
     return 'Hello World'
   },
+  user({ id }) {
+    return fakeDb.users.find(user => user.id === Number(id))
+  },
 }
 
 const app = express()
 app.use(bodyParser.json())
-app.use('/graphql', (req, res) => {
-  console.log('Query received', req.body.query)
-  graphql(schema, req.body.query, resolvers).then(result => {
-    console.log('result', result)
-    res.send(result)
-  })
-})
+
+app.use('/graphql', graphqlExpress({ schema, rootValue: resolvers }))
+
+app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
 app.listen(4000, () =>
   console.log('App is listening on localhost:4000/graphql')
 )
-
-// Now try this request:
-// curl -X POST \
-//   http://localhost:4000/graphql \
-//   -H 'content-type: application/json' \
-//   -d '{"query":"{hello}"}'
